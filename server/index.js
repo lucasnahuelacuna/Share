15318aa4322f22e3ad338da1a8a600eabb9b2f11
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2021-09-23 18:27:01 -0300

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+const dotenv = require('dotenv')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+dotenv.config()
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(error => console.log(error.message))
