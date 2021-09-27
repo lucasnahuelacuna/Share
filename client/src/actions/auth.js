0fd473055b330264820c813a50f530801cdc49d9
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2021-09-27 17:25:34 -0300

    JWT Login

---
+++ client/src/actions/auth.js
@@ -0,0 +1,20 @@
+import * as api from '../api'
+import { AUTH, LOGOUT } from '../constants/actionTypes'
+
+export const signin = (formData, history) => async (dispatch) => {
+    try {
+        // log in the user
+        history.push('/')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const signup = (formData, history) => async (dispatch) => {
+    try {
+        // sign up the user
+        history.push('/')
+    } catch (error) {
+        console.log(error)
+    }
+}
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,20 +8,36 @@ import { useDispatch } from 'react-redux'
 import Icon from './icon'
 import { AUTH, LOGOUT } from '../../constants/actionTypes'
 import { useHistory } from 'react-router-dom'
+import { signin, signup } from '../../actions/auth'
+
+const initialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 const Auth = () => {
     const classes = useStyles()
     const [showPassword, setShowPassword] = useState(false)
     const [isSignup, setIsSignUp] = useState(false)
+    const [formData, setFormData] = useState(initialState)
     const dispatch = useDispatch()
     const history = useHistory()
     
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        if(isSignup) {
+            dispatch(signup(formData, history))
+        } else {
+            dispatch(signin(formData, history))
+        }
     }
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     const handleShowPassword = () => 
