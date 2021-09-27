489b552537ddb3160910a8bcd6b90c41393129fe
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2021-09-27 18:36:31 -0300

    JWT Auth Backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/users')
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(express.json())
 app.use(cors())
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
 dotenv.config()
 
